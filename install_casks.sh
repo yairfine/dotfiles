a7533741652270ef51c9f06928611dc886556740
Yaw <contact4192@gmail.com>
2022-12-31 12:20:19 +0200

    added brew casks apps install support

---
+++ install_casks.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DOT_FILES_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+echo "INSTALLING CASKS APPS ON YOUR SYSTEM"
+
+if command -v brew &>/dev/null; then
+    brew update
+    xargs brew install --cask < "$DOT_FILES_DIR/casks.txt"
+else
+    echo "Homebrew could not be found, skipping casks apps"
+fi
