4c5f931971d05c7ad7ea6a1a06e268070f96019a
Yaw Han <contact4192@gmail.com>
2025-01-05 12:08:15 +0200

    Update setup script to use stow instead of copying
    Also removed all other setup scripts from setup.sh
    besides stow dotfiles and git-configure.
    These setup scripts are used rarely and can be executed
    manually when needed

--- setup.sh
+++ setup.sh
@@ -2,77 +2,30 @@
 
 DOT_FILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-copy_dotfiles() {
-    echo "COPYING ENVIRONMENT FILES TO HOME DIR";
+stow_dotfiles() {
+    if ! command -v stow 2 >&1 >/dev/null; then
+        echo "stow could not be found"
+        exit 1
+    fi
 
-    for file in $DOT_FILES_DIR/.{bashrc,zshrc,exports,alias,functions,zsh_prompt,bash_prompt,clang-format,activate_brew,bash_specific}; do
-        [ -r "$file" ] && [ -f "$file" ] && cp -v "$file" $HOME
-    done;
-
-    echo "export DOT_FILES_DIR=${DOT_FILES_DIR}" >> $HOME/.exports
-    unset file;
+    echo "INSTALLING ENVIRONMENT FILES TO HOME DIR"
+    stow -S sh -S bash -S zsh -S clang-format -S macos
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	copy_dotfiles;
-    $DOT_FILES_DIR/git-configure;
+    stow_dotfiles
+    $DOT_FILES_DIR/git-configure.sh
 else
-	read -p "This may overwrite existing dotfiles in your home directory. Are you sure? (y/N) ";
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		copy_dotfiles;
-        $DOT_FILES_DIR/git-configure;
-	fi;
-fi;
-unset copy_dotfiles;
+    read -p "This may overwrite existing dotfiles in your home directory. Are you sure? (y/N) "
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        stow_dotfiles
+        $DOT_FILES_DIR/git-configure.sh
+    fi
+fi
+unset stow_dotfiles
 
 source $HOME/.zshrc
-
-read -p "Install git scripts? (y/N) "
-echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    sudo ./git-scripts/install.sh
-fi
-
-chmod u+x "$DOT_FILES_DIR/git-setup-identity.sh"
-if [ "$need_git_id_setup" = true ] ; then
-    "$DOT_FILES_DIR/git-setup-identity.sh"
-else
-    read -p "Setup new Git identity? (y/N) ";
-    echo "";
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        "$DOT_FILES_DIR/git-setup-identity.sh"
-    fi;
-fi;
-
-chmod u+x "$DOT_FILES_DIR/macos.sh"
-read -p "Configure MacOS defaults writes? (y/N) ";
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    "$DOT_FILES_DIR/macos.sh"
-fi;
-
-
-read -p "Install Homebrew? (y/N) ";
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    /bin/bash .activate_brew
-fi;
-
-chmod u+x "$DOT_FILES_DIR/installations.sh"
-read -p "Install brew and pip packages? (y/N) ";
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    "$DOT_FILES_DIR/installations.sh"
-fi;
-
-chmod u+x "$DOT_FILES_DIR/install_casks.sh"
-read -p "Install brew cask apps packages? (y/N) ";
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    "$DOT_FILES_DIR/install_casks.sh"
-fi;
 
 echo "Done, now run the following command:"
 echo "source $HOME/.zshrc"
