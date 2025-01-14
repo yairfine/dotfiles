f1128432a7230fcaeba36b89a8a6d507c5fff177
Yaw Han <contact4192@gmail.com>
2025-01-14 11:13:02 +0200

    rename variable

--- install_casks.sh
+++ install_casks.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-DOT_FILES_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+DOTFILES_DIT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 echo "INSTALLING CASKS APPS ON YOUR SYSTEM"
 
 if command -v brew &>/dev/null; then
     brew update
-    xargs brew install --cask < "$DOT_FILES_DIR/casks.txt"
+    xargs brew install --cask < "$DOTFILES_DIT/casks.txt"
 else
     echo "Homebrew could not be found, skipping casks apps"
 fi
--- installations.sh
+++ installations.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 
-DOT_FILES_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+DOTFILES_DIT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 echo "INSTALLING PACKAGES ON YOUR SYSTEM"
 
 if command -v python3 &>/dev/null; then
     python3 -m pip install --upgrade pip
-    python3 -m pip install -r "$DOT_FILES_DIR/requirements.txt"
+    python3 -m pip install -r "$DOTFILES_DIT/requirements.txt"
 else
     echo "python3 could not be found, skipping pip packages"
 fi
 
 if command -v brew &>/dev/null; then
     brew update
-    xargs brew install <"$DOT_FILES_DIR/brews.txt"
+    xargs brew install <"$DOTFILES_DIT/brews.txt"
     git lfs install
 else
     echo "Homebrew could not be found, skipping brew packages"
